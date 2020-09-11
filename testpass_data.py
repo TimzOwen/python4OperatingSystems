6db50ccd54966ff8a2bd4d26005de4057e2d55de
Timz Owen <ktimothy@kabarak.ac.ke>
2020-09-11 10:56:11 +0300

    Create testpass_data.py
    data pass test installation

---
+++ testpass_data.py
@@ -0,0 +1,17 @@
+# linux and windows test for py
+
+# print logs and len in google web site
+import requests
+
+response = requests.get("https://www.google.com")
+len(response.text)
+
+# DataFrames
+import pandas
+
+visitors = [123,456,789,987,654,312,159]
+errors = [12,23,45,56,78,89,13]
+df = pandas.DataFrame({"visitors":visitors,"errors":errors},index=["Mon","Tue","Wed","Thur","Fri","Sat","Sun"])
+print(df)
+df["errors"].mean() #clean data to find mean.
+print(df) #gives the mean
