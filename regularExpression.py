a1efe7006eeebc47d6ae6c264c6c09e8dee4e32c
Timz Owen <ktimothy@kabarak.ac.ke>
2020-09-16 23:17:17 +0300

    Update regularExpression.py
    regular expression

--- regularExpression.py
+++ regularExpression.py
@@ -1,1 +1,28 @@
+import re
 
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
+
+vtxt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+
+
+
+line = "Cats are smarter than dogs";
+
+matchObj = re.match( r'dogs', line, re.M|re.I)
+if matchObj:
+   print "match --> matchObj.group() : ", matchObj.group()
+else:
+   print "No match!!"
+
+searchObj = re.search( r'dogs', line, re.M|re.I)
+if searchObj:
+   print "search --> searchObj.group() : ", searchObj.group()
+else:
+   print "Nothing found!!"
