46c10e753162d6045cb7463e3fe54003d194f936
Timz Owen <ktimothy@kabarak.ac.ke>
2020-09-18 22:05:11 +0300

    Create automation.py
    error fix and debug

---
+++ automation.py
@@ -0,0 +1,19 @@
+# Find and fix the error
+#!/usr/bin/env python3
+import numpy as np
+
+def numpyArray():
+    x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    y = numpy.array([[3, 6, 2], [9, 12, 8]], np.int32)
+    return x*y
+print(numpyArray())
+
+# soln 
+#!/usr/bin/env python3
+import numpy as np
+
+def numpyArray():
+    x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    y = np.array([[3, 6, 2], [9, 12, 8]], np.int32) #make sure numpy var is correct
+    return x*y
+print(numpyArray())
