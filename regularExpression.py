25e33a61c9db1bd7544f7cada2eb983af2a118ff
Timz Owen <ktimothy@kabarak.ac.ke>
2020-09-23 22:22:08 +0300

    Update regularExpression.py
    multiple regx py

--- regularExpression.py
+++ regularExpression.py
@@ -26,3 +26,27 @@ if searchObj:
    print "search --> searchObj.group() : ", searchObj.group()
 else:
    print "Nothing found!!"
+   
+   
+   >>> import re
+>>> s = '(zyx)bc'
+>>> print (re.findall(r'(?<=\()\w+(?=\))|\w', s))
+['zyx', 'b', 'c']
+
+(?<=\() // lookbehind for left parenthesis
+\w+     // all characters until:
+(?=\))  // lookahead for right parenthesis
+|       // OR
+\w      // any character
+
+a = re.findall(r'\((.*?)\)|(.)', '(zyx)bc',re.DEBUG); a
+[('zyx', ''), ('', 'b'), ('', 'c')]
+branch 
+  literal 40 
+  subpattern 1 
+    min_repeat 0 65535 
+      any None 
+  literal 41 
+or
+  subpattern 2 
+    any None
