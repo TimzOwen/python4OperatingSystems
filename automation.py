dd4aa4fad272094bdbde86930494fd33d7aa79a3
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-03 01:01:58 +0300

    Update automation.py
    function to check cpu and os

--- automation.py
+++ automation.py
@@ -24,3 +24,27 @@ import shutil
 du = shutil.disk_usage("/")
 print(du) # usage(total=214605754368, used=203108868096, free=11496886272)
 
+
+
+#Calculate CPU usage
+import psutil
+psutil.cpu_percentage(0.1)
+
+#FUCNTION TO CHECK FOR CPU AND OS USAGE:
+
+#!/user/bin/ env python3
+
+import shutil
+import psutil
+
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free/du.total*100
+    return free>20
+def check_Cpu_usage():
+    usage = psutil.cpu_percentage(1)
+    return usage <75
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("ERROR!!!")
+else:
+    print("EVERYTHING OK")
