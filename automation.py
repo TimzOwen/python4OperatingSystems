0701e3ae78d993c8dd212f59e0dae4beea91e9e5
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-03 00:12:58 +0300

    Update automation.py
    os disk checkup code update

--- automation.py
+++ automation.py
@@ -17,3 +17,10 @@ def numpyArray():
     y = np.array([[3, 6, 2], [9, 12, 8]], np.int32) #make sure numpy var is correct
     return x*y
 print(numpyArray())
+
+
+#Check on Computer usage and free space availability
+import shutil
+du = shutil.disk_usage("/")
+print(du) # usage(total=214605754368, used=203108868096, free=11496886272)
+
