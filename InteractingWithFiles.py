604bb2a4caebc930d3c01a895da099c7ec73e5b7
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-05 23:14:38 +0300

    Update InteractingWithFiles.py
    Reading and manipulating files

--- InteractingWithFiles.py
+++ InteractingWithFiles.py
@@ -190,3 +190,151 @@ for name in os.listdir(dir):
 website/images is a directory
 website/index.html is a file
 
+	
+	 
+
+#The create_python_script function creates a new python script in the current working directory, 
+#adds the line of comments to it declared by the 'comments' variable, 
+#and returns the size of the new file. Fill in the gaps to create a script called "program.py".
+
+import os
+def create_python_script(filename):
+  comments = "# Start of a new Python program"
+  with open(filename,"a") as file:
+    file.write(comments)
+  return os.path.getsize(filename)
+
+
+print(create_python_script("program.py"))
+
+
+#The new_directory function creates a new directory inside the 
+#current working directory, then creates a new empty file inside the new 
+#directory, and returns the list of files in that directory. Fill 
+#in the gaps to create a file "script.py" in the directory "PythonPrograms".
+import os
+
+def new_directory(directory, filename):
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory) == False:
+    ___
+
+  # Create the new file inside of the new directory
+  os.chdir(___)
+  with open (___) as file:
+    pass
+
+  # Return the list of files in the new directory
+  return ___
+
+print(new_directory("PythonPrograms", "script.py"))
+
+#SOLN 
+
+import os
+
+def new_directory(directory, filename):
+  cw = os.getcwd()
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory) == False:
+    os.mkdir(directory)
+
+  # Create the new file inside of the new directory
+  os.chdir(directory)
+  with open (filename,"w+") as file:
+    pass
+  os.chdir(cw)
+  ls = os.listdir(directory)
+
+  # Return the list of files in the new directory
+  return ls
+
+print(new_directory("PythonPrograms", "script.py"))
+
+
+
+#The file_date function creates a new file in the current working directory, 
+#checks the date that the file was modified, and returns just the date portion
+#of the timestamp in the format of yyyy-mm-dd. Fill in the gaps to create a
+#file called "newfile.txt" and check the date that it was modified.
+
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  ___
+  timestamp = ___
+  # Convert the timestamp into a readable format, then into a string
+  ___
+  # Return just the date portion 
+  # Hint: how many characters are in “yyyy-mm-dd”? 
+  return ("{___}".format(___))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
+
+
+#SOLN
+import os
+import datetime
+
+def file_date(filename):
+  # Create the file in the current directory
+  with open(filename, "w+") as file:
+    pass
+  timestamp = os.path.getmtime(filename)
+  # Convert the timestamp into a readable format, then into a string
+  tm = datetime.datetime.fromtimestamp(timestamp).date()
+  # Return just the date portion 
+  # Hint: how many characters are in “yyyy-mm-dd”? 
+  return ("{}".format(tm))
+
+print(file_date("newfile.txt")) 
+# Should be today's date in the format of yyyy-mm-dd
+
+
+
+
+#The parent_directory function returns the name of the directory that's located 
+#just above the current working directory. 
+#Remember that '..' is a relative path alias that means "go up to the parent directory". 
+#Fill in the gaps to complete this function.
+
+import os
+def parent_directory():
+  # Create a relative path to the parent 
+  # of the current working directory 
+  relative_parent = os.path.join(___, ___)
+
+  # Return the absolute path of the parent directory
+  return ___
+
+print(parent_directory())
+
+
+#SOLN 
+import os
+def parent_directory():
+  # Create a relative path to the parent 
+  # of the current working directory 
+  relative_parent = os.path.join(os.getcwd(), "..")
+
+  # Return the absolute path of the parent directory
+  return os.path.abspath(relative_parent)
+
+print(parent_directory())
+
+
+#READING AND MANIPULATING FILES WITH CSV
+import csv 
+#open the file 
+f = open("csv_file.txt")
+scv_f = csv.reader(f)
+#iterate throught the data in the list
+for row in csv_f:
+    name,phone,role = row
+    print("Name:{}, phone:{},Role: {}".format(name,phone,role)) #print the data in the same format as specified
+f.close()
+
+
