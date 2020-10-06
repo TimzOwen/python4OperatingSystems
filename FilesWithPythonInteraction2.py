8b429a74da1181f67cb85d076fd26564ad00467f
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-06 12:42:50 +0300

    Update FilesWithPythonInteraction2.py
    interacting with CSV files update

--- FilesWithPythonInteraction2.py
+++ FilesWithPythonInteraction2.py
@@ -1,1 +1,193 @@
 
+#GENERATING CSV FILES
+#create a list of the data to be saved and stored
+hosts = [["workstation.local","192.168.25.46"],["websever.cloud", "10.2.5.6"]]
+with open(hosts.csv ,"w") as host_csv:
+    writer = csv.writer(host_csv)
+    writer.writerows(hosts)
+    
+ 
+#READING AND WRITING FILES WITH DICTIONARIES
+#case of dicReader
+with open('software.csv') as software:
+    reader = csv.dicReader(software)
+    for row in reader:
+        print(("{} has {} users").format(row["name"],row["users"]))
+        
+
+#case 2
+#create a list
+users = [{"name": "sol mansi", .......}] 
+keys = ["name", "username","department"]
+with open('by_department.csv','w') as by_department:
+    writer = csv.DictWriter(by_department,fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(users) #prints data to teh formar./use ....cat by_department.csv 
+  
+
+#RECAP QUIZ PRACTICE
+#We're working with a list of flowers and some information about each one. The create_file function writes this
+#information to a CSV file. The contents_of_file function reads this file into records and returns the information in
+# a nicely formatted block. Fill in the gaps of the contents_of_file function to turn the data in the CSV 
+#file into a dictionary using DictReader.
+
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  ___
+    # Read the rows of the file into a dictionary
+    ___
+    # Process each item of the dictionary
+    for ___:
+      return_string += "a {} {} is {}\n".format(row["color"], row["name"], row["type"])
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+
+
+#SOLN 
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  with open(filename) as file:
+    # Read the rows of the file into a dictionary
+    f = csv.DictReader(file)
+    # Process each item of the dictionary
+    for row in f:
+      return_string += "a {} {} is {}\n".format(row["color"], row["name"], row["type"])
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+#Output 
+a pink carnation is annual
+a yellow daffodil is perennial
+a blue iris is perennial
+a red poinsettia is perennial
+a yellow sunflower is annual
+
+
+
+
+
+#Using the CSV file of flowers again, fill in the gaps of the contents_of_file function to process the data without turning 
+#it into a dictionary. How do you skip over the header record with the field names?
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  ___
+    # Read the rows of the file
+    rows = ___
+    # Process each row
+    for row in rows:
+      ___ = row
+      # Format the return string for data rows only
+
+          return_string += "a {} {} is {}\n".format(___)
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+
+#SOLN 
+  import os
+  import csv
+
+  # Create a file with data in it
+  def create_file(filename):
+    with open(filename, "w") as file:
+      file.write("name,color,type\n")
+      file.write("carnation,pink,annual\n")
+      file.write("daffodil,yellow,perennial\n")
+      file.write("iris,blue,perennial\n")
+      file.write("poinsettia,red,perennial\n")
+      file.write("sunflower,yellow,annual\n")
+
+  # Read the file contents and format the information about each row
+  def contents_of_file(filename):
+    return_string = ""
+
+    # Call the function to create the file 
+    create_file(filename)
+
+    # Open the file
+    with open(filename) as f:
+      # Read the rows of the file
+      rows = csv.reader(f)
+      headers = next(rows)
+      # Process each row
+      for row in rows:
+        name, color, ty = row
+        # Format the return string for data rows only
+
+        return_string += "a {} {} is {}\n".format(name, color, ty)
+    return return_string
+
+  #Call the function
+  print(contents_of_file("flowers.csv"))
+
+#output
+a carnation pink is annual
+a daffodil yellow is perennial
+a daffodil yellow is perennial
+a iris blue is perennial
+a poinsettia red is perennial
+a sunflower yellow is annual
