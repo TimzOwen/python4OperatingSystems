d290d7a977e2c6237659b182c1097c6990945889
Timz Owen <ktimothy@kabarak.ac.ke>
2020-10-15 11:50:06 +0300

    Update regularExpression.py
    regex basic update

--- regularExpression.py
+++ regularExpression.py
@@ -50,3 +50,47 @@ branch
 or
   subpattern 2 
     any None
+      
+      
+      
+
+#REGULAR EXPRESSION
+import re
+log = "July 31 07:51:48 my computer bad_process=[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
+
+#example 2
+#!/usr/bin/python
+import re
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print "matchObj.group() : ", matchObj.group()
+   print "matchObj.group(1) : ", matchObj.group(1)
+   print "matchObj.group(2) : ", matchObj.group(2)
+else:
+   print "No match!!"
+
+matchObj.group() :  Cats are smarter than dogs
+matchObj.group(1) :  Cats
+matchObj.group(2) :  smarter
+
+#Basic matching with grep
+
+grep  thon /user/share/file  #prints files with *thon
+
+#.dot
+grep l.rts /usr/shre/dict/words #[prints all words with the characters mentioned]
+
+# ^ caret indicate start of a word
+# $ dollar sign indicate end of a word
+
+grep ^ fruit /user/share/dict/wordchars
+grep cat$ /user/share/dict/word
+      
+
