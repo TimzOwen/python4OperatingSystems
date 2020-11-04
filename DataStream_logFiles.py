087ad3bc65427b4024b41fa1682f7f86a768830f
Timz Owen <ktimothy@kabarak.ac.ke>
2020-11-04 16:00:37 +0300

    Update DataStream_logFiles.py
    loggin files in python

--- DataStream_logFiles.py
+++ DataStream_logFiles.py
@@ -106,7 +106,68 @@ results = subprocess.run(["rm","Does not exist"], capture_error=true) # 1 = fail
 print(results.stdout)
 print(results.stderr)
 
-#
+
+ ==================================================================================================Readme
+# Python subprocesses
+
+#changin path variable
+import os
+import subprocess
+
+my_env = os.environ.copy()
+my_env["PATH"] = os.pathsep.join(["/opt/myapp/", my_env["PATH"]])
+
+results = subprocess.run(["myapp"], env=my_env)
+
+
+# Processing Log Files
+# Filtering Log Files with Regular expression
+
+import sys
+logfile = sys.argv[1]
+with open('logfile') as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+          # regex.
+        import re
+
+        pattern = r"USER \((\w+)\)$"
+        liner = "Random user and server log message with the user name to be fecthed by the user"
+        result = re.search(pattern, line)
+        print(result[1]) # shpws the log file
+        print(line.strip()) # create a regulax ti be used here
+
+
+
+
+
+# We're using the same syslog, and we want to display the date, time, and process id that's 
+# inside the square brackets. We can read each line of the syslog and pass the contents 
+# to the show_time_of_pid function. Fill in the gaps 
+# to extract the date, time, and process id from the passed line, and return this format: Jul 6 14:01:23 pid:29440.
+
+#  soln
+import re
+def show_time_of_pid(line):
+  pattern = pattern = r"^(\w{3}\s\d\s\d+:\d+:\d+).*\[(\d+)"  # or r"([\w :]{14}).*\[(\d+)" or #    pattern = r"(\w+ \d? \d+:\d+:\d+).*\[(\d{5})\]"
+  result = re.search(pattern, line) 
+  return "{} pid:{}".format(result[1], result[2])
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+
+print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+
+print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
+
 
 
 
